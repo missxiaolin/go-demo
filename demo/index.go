12aa4c7c7296f2120fc5b36409b342fe3c8e8d85
xiaolin <462441355@qq.com>
2018-04-05 11:34:34 +0800

    first commit

---
+++ demo/index.go
@@ -0,0 +1,29 @@
+package demo
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+)
+
+func variable()  {
+	var a int
+	var s string
+	fmt.Println(a,s)
+}
+
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name
+	fmt.Printf("Calling function %s with args" + "(%d, $d)", opName, a, b)
+	return op(a, b)
+}
+
+func pow(a,b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Hellow")
+	fmt.Println(apply(pow,3,4))
+}
